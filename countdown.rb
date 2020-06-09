e13aba613e1c635bd508d4f125b7cea470cf9002
Tyler Williams <tyler.williams2996@gmail.com>
2020-06-09 03:21:24 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,18 @@
-#write your code here
 
-def countdown
+
+number_of_seconds = 10
+
+
+def countdown(number_of_seconds)
+while number_of_seconds > 0
+puts "#{number_of_seconds} SECOND(S)!"
+number_of_seconds -= 1 
 end
+return "HAPPY NEW YEAR!"
+
+
+end
+def countdown_with_sleep(number_of_seconds)
+  sleep(number_of_seconds)
+end
+countdown_with_sleep(number_of_seconds)
